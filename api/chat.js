984ecd7f4ba8f685843dd7b15459e175d802aa2a
Allison <110505943+leeallix@users.noreply.github.com>
2026-02-17 20:12:58 -0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -42,4 +42,4 @@ export default async function handler(req, res) {
     console.error('Error:', error);
     return res.status(500).json({ error: error.message });
   }
-}
+};
